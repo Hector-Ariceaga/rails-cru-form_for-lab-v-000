87ad99361fb29da9678a815dc01279b58b280168
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-06 17:00:59 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,39 +1,40 @@
 class GenresController < ApplicationController
-
-  def index
+  
+  def index 
     @genres = Genre.all
-  end
-
-  def show
+  end 
+  
+  def show 
     genre_find
-  end
-
-  def new
+  end 
+  
+  def new 
     @genre = Genre.new
-  end
-
-  def create
+  end 
+  
+  def create 
     @genre = Genre.create!(genre_params)
     redirect_to @genre
   end
-
-  def edit
+  
+  def edit 
     genre_find
   end
-
-  def update
+  
+  def update 
     genre_find
     @genre.update(genre_params)
+    redirect_to @genre
   end
-
-  private
-
+  
+  private 
+  
   def genre_params
     params.require(:genre).permit(:name)
   end
-
+  
   def genre_find
     @genre = Genre.find(params[:id])
-  end
-
+  end 
+  
 end
