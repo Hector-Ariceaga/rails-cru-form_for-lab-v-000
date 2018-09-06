3c09c150bb4ec75e370730c69c1136919013f73f
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-06 16:55:18 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,14 +1,38 @@
 class GenresController < ApplicationController
   
   def index 
+    @genres = Genre.all
   end 
   
   def show 
+    genre_find
   end 
   
   def new 
+    @genre = Genre.new
   end 
   
   def create 
+    @genre = Genre.create!(genre_params)
   end
+  
+  def edit 
+    genre_find
+  end
+  
+  def update 
+    genre_find
+    @genre.update(genre_params)
+  end
+  
+  private 
+  
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
+  def genre_find
+    @genre = Genre.find(params[:id])
+  end 
+  
 end
